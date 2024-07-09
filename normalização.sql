8b88904eab13e1e366e84599038e560b7ba78e7d
Hilton Elias <149949475+tomaszeswisc@users.noreply.github.com>
2024-07-09 10:50:00 -0300

    Add files via upload
    update

--- normalização.sql
+++ normalização.sql
@@ -1,5 +1,8 @@
 -- Criação e Inserção dos Dados nas Tabelas Não Normalizadas
 -- Tabela clientes com Compras Não Atômicas: Esta tabela viola a Primeira Forma Normal (1FN) porque a coluna compras contém múltiplos valores, o que não é atômico.
+create database db_normalizacao;
+
+use db_normalizacao;
 
 CREATE TABLE clientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -43,3 +46,59 @@ CREATE TABLE produtos (
 INSERT INTO produtos (nome_produto, fornecedor, telefone_fornecedor) VALUES
 ('TV', 'LG', '12345678'),
 ('Geladeira', 'Samsung', '87654321');
+
+-- Normalizando o Banco de dados 1FN
+
+CREATE TABLE clientes_1FN (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    telefone VARCHAR(15)
+);
+
+CREATE TABLE compras (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT,
+    item VARCHAR(100),
+    FOREIGN KEY (cliente_id) REFERENCES clientes_1FN(id)
+);
+
+-- Inserir dados na nova estrutura
+
+INSERT INTO clientes_1FN (nome, telefone)
+SELECT nome, telefone FROM clientes;
+
+
+INSERT INTO compras (cliente_id, item) VALUES
+(1, 'TV'), (1, 'Geladeira'), (2, 'Máquina de Lavar');
+
+-- Excluir a tabela não Normalizada
+DROP TABLE clientes;
+
+
+-- Normalizando o Banco de dados 2FN
+
+CREATE TABLE clientes_2FN (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100)
+);
+
+CREATE TABLE pedidos_2FN (
+    pedido_id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT,
+    data_pedido DATE,
+    valor DECIMAL(10, 2),
+    FOREIGN KEY (cliente_id) REFERENCES clientes_2FN(id)
+);
+
+-- Inserir dados na nova estrutura
+
+INSERT INTO clientes_2FN (id, nome) 
+SELECT cliente_id, nome_cliente FROM pedidos GROUP BY cliente_id, nome_cliente;
+
+INSERT INTO pedidos_2FN (cliente_id, data_pedido, valor)
+SELECT cliente_id, data_pedido, valor FROM pedidos;
+
+
+-- Inserir dados na nova estrutura
+
+DROP TABLE pedidos;
